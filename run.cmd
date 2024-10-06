43e4302368137a1c37186446dd609f0675953c21
Nicholas Poh <programatix@msn.com>
2024-10-06 08:02:24 +0800

    Added a batch file to run the projects.

---
+++ run.cmd
@@ -0,0 +1,3 @@
+start dotnet run --project ElsaServer --urls "https://localhost:5001"
+start dotnet run --project ElsaStudioBlazorServer --urls "https://localhost:7001"
+start https://localhost:7001
